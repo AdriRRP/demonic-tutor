f8e346d01697831ee979bd8d6a3d393d81c4358d
Adrian Ramos <adrianramosrp@gmail.com>
2026-03-21 17:20:18 +0100

    refactor(ids): centralize shared string backing semantics

--- src/domain/play/ids.rs
+++ src/domain/play/ids.rs
@@ -1,138 +1,60 @@
 use std::sync::Arc;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct GameId(pub Arc<str>);
+// Centralize the backing storage for domain ids so we can revisit the choice
+// later without touching every id type. We intentionally keep `Arc<str>` today
+// because ids are cloned pervasively into events and tests, and we do not yet
+// have profiling that justifies a broader representation change.
+type SharedIdStr = Arc<str>;
 
-impl GameId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
+macro_rules! shared_string_id {
+    ($name:ident, display) => {
+        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        pub struct $name(pub SharedIdStr);
+
+        impl $name {
+            pub fn new(value: impl Into<String>) -> Self {
+                Self(SharedIdStr::from(value.into()))
+            }
+
+            #[must_use]
+            pub fn as_str(&self) -> &str {
+                &self.0
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(value: String) -> Self {
+                Self(SharedIdStr::from(value))
+            }
+        }
+
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
+    ($name:ident) => {
+        #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+        pub struct $name(pub SharedIdStr);
+
+        impl $name {
+            pub fn new(value: impl Into<String>) -> Self {
+                Self(SharedIdStr::from(value.into()))
+            }
+        }
+
+        impl From<String> for $name {
+            fn from(value: String) -> Self {
+                Self(SharedIdStr::from(value))
+            }
+        }
+    };
 }
 
-impl From<String> for GameId {
-    fn from(s: String) -> Self {
-        Self(Arc::from(s))
-    }
-}
-
-impl std::fmt::Display for GameId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl GameId {
-    #[must_use]
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct PlayerId(pub Arc<str>);
-
-impl PlayerId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
-}
-
-impl From<String> for PlayerId {
-    fn from(s: String) -> Self {
-        Self(Arc::from(s))
-    }
-}
-
-impl std::fmt::Display for PlayerId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl PlayerId {
-    #[must_use]
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct DeckId(pub Arc<str>);
-
-impl DeckId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
-}
-
-impl From<String> for DeckId {
-    fn from(s: String) -> Self {
-        Self(Arc::from(s))
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct CardInstanceId(Arc<str>);
-
-impl CardInstanceId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
-}
-
-impl std::fmt::Display for CardInstanceId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl CardInstanceId {
-    #[must_use]
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct CardDefinitionId(Arc<str>);
-
-impl CardDefinitionId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
-}
-
-impl std::fmt::Display for CardDefinitionId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl CardDefinitionId {
-    #[must_use]
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct StackObjectId(Arc<str>);
-
-impl StackObjectId {
-    pub fn new(value: impl Into<String>) -> Self {
-        Self(Arc::from(value.into()))
-    }
-}
-
-impl std::fmt::Display for StackObjectId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl StackObjectId {
-    #[must_use]
-    pub fn as_str(&self) -> &str {
-        &self.0
-    }
-}
+shared_string_id!(GameId, display);
+shared_string_id!(PlayerId, display);
+shared_string_id!(DeckId);
+shared_string_id!(CardInstanceId, display);
+shared_string_id!(CardDefinitionId, display);
+shared_string_id!(StackObjectId, display);
